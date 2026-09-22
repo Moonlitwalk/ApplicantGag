74b58206312bf12ace40bc2403cae4232f077098
Moonlitwalk <sendmeforthmoonlitwalk@gmail.com>
2026-09-22 11:13:21 +0200

    utf8 encoding enforced

--- src/main/java/com/aboutme/notTheUsualCV/controller/ApplicantController.java
+++ src/main/java/com/aboutme/notTheUsualCV/controller/ApplicantController.java
@@ -1,23 +1,29 @@
 package com.aboutme.notTheUsualCV.controller;
 
 import com.aboutme.notTheUsualCV.model.entity.dto.ApplicantResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 
 @RestController
 
 public class ApplicantController {
 
-    @GetMapping("/api/mats")
+    @GetMapping(
+            value="/api/mats",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ApplicantResponse getApplicant(){
         return new ApplicantResponse(
                 "Mats Arras",
                 "Technical SAP Specialist",
                 31,
                 "Junior SAP BTP Consultant / Architect",
-                "Duesseldorf",
-                "Hallo Frau Brar und liebes FORTIS Team, über ein Gespräch freue ich mich sehr!"
+                "Düsseldorf",
+                "Hallo Frau Brar und liebes FORTIS Team, ich freue mich auf ein Gespräch!"
         );
     }
 
